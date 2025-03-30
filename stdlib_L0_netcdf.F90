0ee3d362a5195e1c92315e50a89e54f8314a1840
Francesco Tucciarone <43784914+ftucciarone@users.noreply.github.com>
2025-03-30 22:51:21 +0200

    Create stdlib_L0_netcdf.F90

---
+++ stdlib_L0_netcdf.F90
@@ -0,0 +1,18 @@
+!
+! Level 0 NetCDF wrappers: No checks are performed, except
+!                          the error check on exit
+!
+module stdlib_L0_netcdf
+
+   USE stdlib_L0_netcdf_datasets
+   USE stdlib_L0_netcdf_dimensions
+   USE stdlib_L0_netcdf_variables
+
+   implicit none
+
+contains 
+   !
+   ! 
+   !
+   !
+end module stdlib_L0_netcdf
