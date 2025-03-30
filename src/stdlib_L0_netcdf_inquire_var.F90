9a0feb3855fba274e1cb1719f18b508bdafcad2c
Francesco Tucciarone <43784914+ftucciarone@users.noreply.github.com>
2025-03-30 20:39:59 +0200

    Update stdlib_L0_netcdf_inquire_var.F90

--- src/stdlib_L0_netcdf_inquire_var.F90
+++ src/stdlib_L0_netcdf_inquire_var.F90
@@ -2,7 +2,7 @@
 ! Level 0 NetCDF wrappers: No checks are performed, except
 !                          the error check on exit
 !
-submodule (stdlib_netcdf) stdlib_L0_netcdf_inquire_var
+module stdlib_L0_netcdf_inquire_var
 
     implicit none
 
@@ -11,11 +11,11 @@ contains
    !   Routine: netCDF_inquire_var
    !
    subroutine netCDF_inquire_var( ncId, varID, varName, xtype, nDims, dimsIDs, nAtts )
-      USE netcdf, only: nf90_inquire_var, f90_NoErr
+      USE netcdf, only: nf90_inquire_variable
       !
       integer,                    intent(in   ) :: ncID
       integer,                    intent(in   ) :: varID
-      character(len=:), optional, intent(  out) :: varName 
+      character(len=*), optional, intent(  out) :: varName 
       integer,          optional, intent(  out) :: xtype
       integer,          optional, intent(  out) :: nDims
       integer,          optional, intent(  out) :: dimsIDs(:)
@@ -23,9 +23,9 @@ contains
       !
       integer :: ierr
       !
-      ierr = nf90_inquire_var( ncId, varID, varName, xtype, nDims, dimsIDs, nAtts )
-      if ( ierr /= f90_NoErr ) ERROR STOP
+      ierr = nf90_inquire_variable( ncId, varID, varName, xtype, nDims, dimsIDs, nAtts )
+      if ( ierr /= 0 ) ERROR STOP
       !
    end subroutine netCDF_inquire_var
    !
-end submodule stdlib_L0_netcdf_inquire_var
+end module stdlib_L0_netcdf_inquire_var
