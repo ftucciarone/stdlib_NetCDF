0e30549262edc8df71f9225e378249e2aa850338
Francesco Tucciarone <43784914+ftucciarone@users.noreply.github.com>
2025-03-30 20:17:56 +0200

    Create stdlib_L0_netcdf_def_dim.F90

---
+++ src/stdlib_L0_netcdf_def_dim.F90
@@ -0,0 +1,28 @@
+!
+! Level 0 NetCDF wrappers: No checks are performed, except
+!                          the error check on exit
+!
+module stdlib_L0_netcdf_def_dim
+
+    implicit none
+
+contains 
+   !
+   !   Routine: netCDF_def_dim
+   !
+   subroutine netCDF_def_dim( ncId, dimName, len, dimID )
+      USE netcdf, only: nf90_def_dim
+      !
+      integer,          intent(in   ) :: ncID
+      character(len=*), intent(in   ) :: dimName 
+      integer,          intent(in   ) :: len
+      integer,          intent(  out) :: dimID
+      !
+      integer :: ierr
+      !
+      ierr = nf90_def_dim( ncId, dimName, dimId, dimID )
+      if ( ierr /= 0 ) ERROR STOP
+      !
+   end subroutine netCDF_def_dim
+   !
+end module stdlib_L0_netcdf_def_dim
