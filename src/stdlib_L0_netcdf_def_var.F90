b3deb8552ebe0b33d78236c95d9d3b09cd42866a
Francesco Tucciarone <43784914+ftucciarone@users.noreply.github.com>
2025-03-30 14:57:59 +0200

    Create stdlib_L0_netcdf_def_var.F90

---
+++ src/stdlib_L0_netcdf_def_var.F90
@@ -0,0 +1,29 @@
+!
+! Level 0 NetCDF wrappers: No checks are performed, except
+!                          the error check on exit
+!
+submodule (stdlib_netcdf) stdlib_L0_netcdf_def_var
+
+    implicit none
+
+contains 
+   !
+   !   Routine: netCDF_def_var
+   !
+   subroutine netCDF_def_var( ncId, varName, xtype, dimIds, varID )
+      USE netcdf, only: nf90_def_var, f90_NoErr
+      !
+      integer,          intent(in   ) :: ncID
+      character(len=:), intent(in   ) :: varName 
+      integer,          intent(in   ) :: xtype
+      integer,          intent(in   ) :: dimIds(:) 
+      integer,          intent(  out) :: varID
+      !
+      integer :: ierr
+      !
+      ierr = nf90_def_var( ncId, varName, xtype, dimIds, varID )
+      if ( ierr /= f90_NoErr ) ERROR STOP
+      !
+   end subroutine netCDF_def_var
+   !
+end submodule stdlib_L0_netcdf_def_var
