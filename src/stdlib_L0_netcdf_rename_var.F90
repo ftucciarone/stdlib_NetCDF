7c383aebd013e9a397864dd036b3d09496dbedbd
Francesco Tucciarone <43784914+ftucciarone@users.noreply.github.com>
2025-03-30 20:42:43 +0200

    Update stdlib_L0_netcdf_rename_var.F90

--- src/stdlib_L0_netcdf_rename_var.F90
+++ src/stdlib_L0_netcdf_rename_var.F90
@@ -2,7 +2,7 @@
 ! Level 0 NetCDF wrappers: No checks are performed, except
 !                          the error check on exit
 !
-submodule (stdlib_netcdf) stdlib_L0_netcdf_rename_var
+module stdlib_L0_netcdf_rename_var
 
     implicit none
 
@@ -11,17 +11,17 @@ contains
    !   Routine: netCDF_rename_var
    !
    subroutine netCDF_rename_var( ncId, varID, varNewName )
-      USE netcdf, only: nf90_rename_var, f90_NoErr
+      USE netcdf, only: nf90_rename_var
       !
       integer,                    intent(in   ) :: ncID
       integer,                    intent(in   ) :: varID
-      character(len=:), optional, intent(in   ) :: varNewName 
+      character(len=*), optional, intent(in   ) :: varNewName 
       !
       integer :: ierr
       !
       ierr = nf90_rename_var( ncId, varID, varNewName )
-      if ( ierr /= f90_NoErr ) ERROR STOP
+      if ( ierr /= 0 ) ERROR STOP
       !
    end subroutine netCDF_rename_var
    !
-end submodule stdlib_L0_netcdf_rename_var
+end module stdlib_L0_netcdf_rename_var
