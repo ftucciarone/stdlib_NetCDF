9204aded437067cca098dc8217b9935782df17ab
Francesco Tucciarone <43784914+ftucciarone@users.noreply.github.com>
2025-03-30 20:20:49 +0200

    Update stdlib_L0_netcdf_def_var.F90

--- src/stdlib_L0_netcdf_def_var.F90
+++ src/stdlib_L0_netcdf_def_var.F90
@@ -2,7 +2,7 @@
 ! Level 0 NetCDF wrappers: No checks are performed, except
 !                          the error check on exit
 !
-submodule (stdlib_netcdf) stdlib_L0_netcdf_def_var
+module stdlib_L0_netcdf_def_var
 
     implicit none
 
@@ -11,19 +11,19 @@ contains
    !   Routine: netCDF_def_var
    !
    subroutine netCDF_def_var( ncId, varName, xtype, dimIds, varID )
-      USE netcdf, only: nf90_def_var, f90_NoErr
+      USE netcdf, only: nf90_def_var
       !
       integer,          intent(in   ) :: ncID
-      character(len=:), intent(in   ) :: varName 
+      character(len=*), intent(in   ) :: varName 
       integer,          intent(in   ) :: xtype
       integer,          intent(in   ) :: dimIds(:) 
       integer,          intent(  out) :: varID
       !
       integer :: ierr
       !
-      ierr = nf90_def_var( ncId, varName, xtype, dimIds, varID )
-      if ( ierr /= f90_NoErr ) ERROR STOP
+      ierr = nf90_def_var( ncId, TRIM(varName), xtype, dimIds, varID )
+      if ( ierr /= 0 ) ERROR STOP
       !
    end subroutine netCDF_def_var
    !
-end submodule stdlib_L0_netcdf_def_var
+end module stdlib_L0_netcdf_def_var
